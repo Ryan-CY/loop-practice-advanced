ea39aeb0597fc353154785621cdf61e20dedb8f5
Ryan CY <phillin820109@msn.com>
2022-09-29 09:32:23 +0800

    Initial Commit

---
+++ loop practice-advanced/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loop practice-advanced
+//
+//  Created by Ryan Lin on 2022/9/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
